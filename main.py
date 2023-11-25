fba8599945b9c430e93c22941eb86f4ef17f0cd6
mruax <matvey.chernyakov@yandex.ru>
2023-11-25 15:59:07 +0500

    to reduce amount of code added data input function with validation

--- main.py
+++ main.py
@@ -2,25 +2,39 @@ import logging
 from random import randint
 
 
+def valid_input(in_message, retry_message, cond=False):
+    print(in_message)
+    flag = True
+    result = 0
+    while flag:
+        try:
+            result = int(input(retry_message))
+            if cond:
+                if result >= 1:
+                    flag = False
+                else:
+                    print("Данные введены неверно!")
+            else:
+                if result > 1:
+                    flag = False
+                else:
+                    print("Данные введены неверно!")
+        except Exception as e:
+            print("Данные введены неверно!")
+            logging.error(f"Input data: {e}")
+    return result
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, filename="logs/guess.log",
                         filemode="w", format="%(asctime)s %(levelname)s %(message)s")
     logging.info("Program started")
     print("Добро пожаловать в Угадай-ку!")
 
-    flag = True
-    n = 0
-    print("Введите максимальное число диапазона угадывания:")
-    while flag:
-        try:
-            n = int(input("n = "))
-            if n > 1:
-                flag = False
-        except Exception:
-            print("Данные введены неверно! Ваше число должно быть натуральным и большим чем единица.")
-            logging.error(f"Ошибка ввода данных: {Exception}")
-    logging.info(f"Задано n = {n}")
+    n = valid_input("Введите целое число большее 1 (правую границу диапазона угадывания):", "n = ")
+    logging.info(f"Input n = {n}")
 
-    
+    k = valid_input("Введите количество попыток на угадывание (натуральное число):", "k = ", True)
+    logging.info(f"Input k = {k}")
 
     logging.info("Program ended")
